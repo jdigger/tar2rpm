c047ad44577279ae5c94b63ebec80b328d543ae8
Jim Moore <moore.jim@gmail.com>
2012-01-11 11:07:21 -0700

    refactoring

--- test/tar2rpm_spec.rb
+++ test/tar2rpm_spec.rb
@@ -1,37 +1,57 @@
 require_relative '../tar2rpm'
 require 'tmpdir'
 
-describe "TAR 2 RPM" do
+describe Tar2Rpm do
+  
+  before(:each) do
+    @tar2rpm = Tar2Rpm.new
+    @tarfile = "#{File.dirname(__FILE__)}/simple.tar"
+  end
 
-  it "should extract the tar to the SOURCES directory" do
-    tr = Tar2Rpm.new
-    tarfile = "#{File.dirname(__FILE__)}/simple.tar"
-    tr.tar_content_filenames(tarfile).should == "file1.txt\nfile2.txt\n"
 
+  around(:each) do |example|
     with_tmpdir do |tmpdir|
-      tr.extract_tar(tarfile, tmpdir)
-      file_entries = Dir.entries(tmpdir).grep(/^[^.]/)
-      file_entries.should include('file1.txt')
-      file_entries.should include('file2.txt')
+      @tmpdir = tmpdir
+      example.run
     end
+  end
+
+
+  it "should extract the tar to a directory" do
+    @tar2rpm.tar_content_filenames(@tarfile).should == "file1.txt\nfile2.txt\n"
+
+    @tar2rpm.extract_tar(@tarfile, @tmpdir)
+    file_entries = dir_files(@tmpdir)
+    file_entries.should include('file1.txt')
+    file_entries.should include('file2.txt')
   end
   
 
   it "should create a simple Spec file with the list of files in it"
 
+end
 
-  def with_tmpdir
-    tmpdir = Dir.mktmpdir
-    begin
-      yield(tmpdir)
-    ensure
-      file_entries = Dir.entries(tmpdir).grep(/^[^.]/)
-      file_entries.each {|x|
-        file = "#{tmpdir}/#{x}"
-        File.delete(file)
-      }
-      Dir.rmdir(tmpdir)
-    end
+
+def dir_files(dir)
+  Dir.entries(dir).grep(/^[^.]/)
+end
+
+
+def with_tmpdir
+  tmpdir = Dir.mktmpdir
+  begin
+    yield(tmpdir)
+  ensure
+    rmdir_force(tmpdir)
   end
+end
 
+
+def rmdir_force(dir)
+  file_entries = dir_files(dir)
+  file_entries.each {|x|
+    file = "#{dir}/#{x}"
+    File.delete(file)
+  }
+  Dir.rmdir(dir)
 end
